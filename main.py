841fa209b9e3add5159cfc8b9ad0c2f6149e7d01
draplater <tsjzadmin@gmail.com>
2015-05-28 17:34:49 +0800

    1. 更换模板 2. 添加首页 3. 删除无关的book_form.html

--- main.py
+++ main.py
@@ -117,5 +117,9 @@ def fake_pay_success():
 def css(path):
     return send_from_directory('css', path)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True )
